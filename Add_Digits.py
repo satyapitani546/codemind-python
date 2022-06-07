352e81492aa11850b15a658a873a4b0525e24baf
satyapitani546 <97425791+satyapitani546@users.noreply.github.com>
2022-06-07 23:02:46 +0530

    def sum(n):     re=0     while(n):         r=n%10         re=re+r         n=n//10     return re n=int(input()) while n>9:     n=sum(n) print(n)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,10 +1,10 @@
 def sum(n):
     re=0
     while(n):
-        r=n%10;
-        re=re+r;
-        n=n//10;
-    return re;
+        r=n%10
+        re=re+r
+        n=n//10
+    return re
 n=int(input())
 while n>9:
     n=sum(n)

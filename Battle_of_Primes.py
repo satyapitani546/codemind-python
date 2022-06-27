ead85dc165fee278289af4611598fe52f946923a
satyapitani546 <97425791+satyapitani546@users.noreply.github.com>
2022-06-27 22:10:02 +0530

    def isprime(k):     if k==1:         return False     else:         for j in range(2,k//2+1):             if k%j==0:                 return False         else:             return True a=int(input()) b=int(input()) c=a+b d=c while(True):     c=c+1     if isprime(c):         e=c         print(e-d)         break

---
+++ Battle_of_Primes.py
@@ -0,0 +1,19 @@
+def isprime(k):
+    if k==1:
+        return False
+    else:
+        for j in range(2,k//2+1):
+            if k%j==0:
+                return False
+        else:
+            return True
+a=int(input())
+b=int(input())
+c=a+b
+d=c
+while(True):
+    c=c+1
+    if isprime(c):
+        e=c
+        print(e-d)
+        break

fef4fc78852d6a3922884281d7f4b26e4c16cf78
satyapitani546 <97425791+satyapitani546@users.noreply.github.com>
2022-07-01 22:41:25 +0530

    n=input() s=0 for i in n:     if i=='1':         s+=1     elif i=='2':         s+=2     elif i=='3':         s+=3     elif i=='4':         s+=4     elif i=='5':         s+=5     elif i=='6':         s+=6     elif i=='7':         s+=7     elif i=='8':         s+=8     elif i=='9':         s+=9 print(s)

---
+++ Numbers_in_String.py
@@ -0,0 +1,22 @@
+n=input()
+s=0
+for i in n:
+    if i=='1':
+        s+=1
+    elif i=='2':
+        s+=2
+    elif i=='3':
+        s+=3
+    elif i=='4':
+        s+=4
+    elif i=='5':
+        s+=5
+    elif i=='6':
+        s+=6
+    elif i=='7':
+        s+=7
+    elif i=='8':
+        s+=8
+    elif i=='9':
+        s+=9
+print(s)
